e5075c92cc6fbe0888331b4609fed51ff4aab02f
Cheney Cai <cheneycai2004@hotmail.com>
2024-06-08 17:47:56 -0400

    usb desc

--- common/src/usb_descriptors.c
+++ common/src/usb_descriptors.c
@@ -185,8 +185,8 @@ char serial[2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1];
 char const* string_desc_arr [] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "Tidyrice",                    // 1: Manufacturer
+  "USB Macro Keyboard",          // 2: Product
   serial,                        // 3: Serials, uses the flash ID
 };
 
